6a47199226f19a60ea04f176d02cd3c1a061f885
Lukas Merlin Jürgens <108548830+lukasmerlin@users.noreply.github.com>
2023-04-14 15:22:56 +0200

    Iteration 1

--- src/App.js
+++ src/App.js
@@ -1,10 +1,23 @@
 import './App.css';
 import { Card, Row, Col, Divider, Input, Button } from 'antd';
 import foods from './foods.json';
+import React, { useState } from 'react';
 
 function App() {
+
+  const [foodList, setFoodList] = useState(foods);
+  const firstTenFoodList = foodList.slice(0, 10);
+
   return (
     <div className="App">
+      <h1>IronNutrition</h1>
+      {firstTenFoodList.map((food, index) => (
+      <div>
+        <p><b>{index}</b> - {food.name} </p>
+        <img src={food.image} width={100} />
+      </div>
+      ))}
+
     </div>
   );
 }
